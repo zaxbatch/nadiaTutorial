a7c19d4fdea362e07bcbb2767cfa10695b41dbb4
Zax <sneakjunkmail@gmail.com>
2026-06-09 15:52:16 -0400

    added alert box

--- generate-index.js
+++ generate-index.js
@@ -10,5 +10,8 @@ const html = `
     ${files.map(f => `<li><a href="${f}">${f}</a></li>`).join('\n')}
   </ul>
 </body>
+<script>
+	alert('This is what a page with no index.html looks like. It's just a contents of the folder. Click on the links to see the content.');
+</script>
 </html>`;
 fs.writeFileSync('index.html', html);
